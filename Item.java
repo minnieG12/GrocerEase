eee71a0af623e1dd9a54de9c060f023c33f86442
minnieG12 <154833115+minnieG12@users.noreply.github.com>
2024-10-29 23:49:26 +0530

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Item {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void adjustQuantity(int adjustment) {
+        this.quantity += adjustment;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - $" + price + " (Quantity: " + quantity + ")";
+    }
+}
